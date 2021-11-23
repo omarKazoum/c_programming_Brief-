71902fbcba25e1b8d78d59637075604674d79c1d
omar kazoum <omarKazoum96@gmail.com>
2021-11-23 17:02:03 +0100

    from youcode day 2

--- main.c
+++ main.c
@@ -22,7 +22,9 @@ struct Compte
 int open_accounts_count=0; 
 int optionChoisis;
 struct Compte comptes[MAX_NUMBER_OF_ACCOUNTS];
-
+#define MAX_ACCOUNST_NUMBER_PER_CIN 10 
+int nbrDesComptesTrouvesApresRecheche=-1;
+int idCompteChoisis=-1;
 //fonction utilisées
 void fakeData();
 void printDataSoFar();
@@ -38,12 +40,18 @@ void afficherMenuOperations();
 void afficherMenuOperationDepotRetrait(int op);
 void demmanderLechoix();
 int choisirUnComptePourOperation();
-Compte* chercherComptesParCin(char cin[]);//recherche les comptes ayant le cin donné
+void addToAccount(int accountId,float amount);
+float lireMontanatDeLUtilisateur(int op);
+// takes a cin {char[]}, comptes Compte[]
+void chercherComptesParCin(char cin[],Compte c[]);//recherche les comptes ayant le cin donné
+void afficherMenuAffichage();
 ///////////////////////:start of main function
 int main(){
 	fakeData();
-	//afficheLeMenuPricipale();
-	printDataSoFar();
+	afficheLeMenuPricipale();
+	//int nbrs[]={12,98,0,-1};
+	
+
 	return 0;
 }
 ///////////////////////////end of main function
@@ -69,6 +77,7 @@ void printDataSoFar(){
 			i,comptes[i].id,comptes[i].nomProprietaire,comptes[i].prenomProprietaire,comptes[i].cin,comptes[i].sold);
 	}
 	printf("\n-----------------------------------------------------");
+	system("cls");
 }
 
 void insertAccount(struct Compte c,int position){
@@ -106,7 +115,9 @@ void afficheLeMenuPricipale(){
 			afficherMenuOperations();
 		;break;
 		case 4://Affichage des 
-		
+			clearScreen();
+			header();
+			afficherMenuAffichage();
 		;break;
 		case 5://Fidelisation
 		
@@ -130,7 +141,7 @@ void introduireUnCompte(){
 	printf("\nentrez le numero de la carte nationale d'identite \n");
 	scanf("%s",c.nomProprietaire);
 	printf("\nsold initial \n");
-	scanf("%d",&c.sold);
+	scanf("%f",&c.sold);
 	c.id=open_accounts_count;
 	bool sortir=true;
 	do{
@@ -141,7 +152,6 @@ void introduireUnCompte(){
 			case OUI:
 				comptes[open_accounts_count-1]=c;
 				open_accounts_count++;
-				
 			break;
 			case NON:
 			//ne rien faire
@@ -151,12 +161,11 @@ void introduireUnCompte(){
 			break;
 		}
 	}while (!sortir);
-	
-
 }
 void entrezqqChosePourOvrirMenuPrincipale(){
 	printf("compte bien ajoute tapez qq chose pour revenir au menu principale\n");
-				getch();
+	getch();
+	afficheLeMenuPricipale();
 }
 void introduirePlusieurComptes(){
 	header();
@@ -218,38 +227,137 @@ void afficherMenuOperations(){
 
 }
 void afficherMenuOperationDepotRetrait(int op){
-	//TODO:: costumize depending on the type of the operation
 	int nbrCompteATraiter=choisirUnComptePourOperation();
+	//printf("utlisateura choisis le compte avec id %d",nbrCompteATraiter); 
+	if(nbrCompteATraiter==-1){
+		//si l'utilisateur n'a pas de compte
+		entrezqqChosePourOvrirMenuPrincipale();
+	}else{
+		//un compte existant est selectione on effectue l'opeartion
+		float mt=lireMontanatDeLUtilisateur(op);
+		//printf("\nentred amount is %f",mt);
+		printf("etes vous sur de vouloir %s le montant de %f %s compte avec l'identifiant %d ?\n"
+		,(op==DEPOT?"deposer ":"retirer"),mt,(op==DEPOT?"dans":"du"),nbrCompteATraiter);
+		if(op==RETRAIT)
+			mt=-mt;
+
+		enum Choix c=prendreChoix();
+		switch (c)
+		{
+		case OUI:
+			addToAccount(nbrCompteATraiter,mt);
+			printf("\nle montant a bien ete %s\n",(op==DEPOT?"depose":"retire"));
+			entrezqqChosePourOvrirMenuPrincipale();
+			break;
+		case NON:
+			afficheLeMenuPricipale();
+			break;
+		}
+		
+	}
 }
+float lireMontanatDeLUtilisateur(int op){
+	printf("entrez le montant que vous voulez %s dans le compte ",(op==DEPOT)?"deposer":"retirer");
+	float montant;
+	scanf("%f",&montant);
+	return montant;
+};
 int choisirUnComptePourOperation(){
 	printf("\npour effectuer un operation il faut d'abord choisir un compte svp entrez votre cin");
 	char cin[20];
 	scanf("%s",cin);
-	//TODO:: continue this find the equivalent accoounts with cin
+	Compte comptesTrouves[MAX_ACCOUNST_NUMBER_PER_CIN];
+	chercherComptesParCin(cin,comptesTrouves);
+	CHOISIR_UN_COMPTE:
+	if(nbrDesComptesTrouvesApresRecheche>0){
+			printf("\nchoisir un compte\n");
+			printf("#\tNumero Du Compte\tNom et Prenom\tSold\n");
+		for(int i=0;i<nbrDesComptesTrouvesApresRecheche;i++){
+			printf("%d\t%d\t\t%s %s\t%f\n",i+1,comptesTrouves[i].id,comptesTrouves[i].nomProprietaire,comptesTrouves[i].prenomProprietaire,comptesTrouves[i].sold);
+		}
+		printf("\nentrez le nombre correspondant au compte de votre choix ");
+		lireOption();
+		if(optionChoisis>=1 && optionChoisis<=nbrDesComptesTrouvesApresRecheche){
+			// donc id du compte est valide
+			return optionChoisis-1;
+		}else{
+			//sinon on lui demande de rentrer le nombre correspondant au compte
+			goto CHOISIR_UN_COMPTE;
+		}
+	}else{
+		printf("le client avec le CIN:%s ne dispose pas de compte\n",cin);
+		return -1;
+	}
+
+
 }
-Compte* chercherComptesParCin(char cin[]){
+void chercherComptesParCin(char cin[],Compte cmpts[]){
 	//maintenet on doit savoir le nombre des comptes ayant ce CIN
 	int count=0;
+	nbrDesComptesTrouvesApresRecheche=0;
 	for(int i=0;i<open_accounts_count;i++){
 		if(strcmp(comptes[i].cin,cin)==0)
 			count++;
 	}
-
-	static Compte foundAccounts[count];
 	int insertingIndex=0;
-	for(int i=0;i<open_accounts_count;i++){
+	for(int i=0;i<open_accounts_count&& i<MAX_ACCOUNST_NUMBER_PER_CIN;i++){
 		if(strcmp(comptes[i].cin,cin)==0){
-			foundAccounts[insertingIndex++]=comptes[i];
+			cmpts[insertingIndex]=comptes[i];
+			insertingIndex++;
 		}
 	}
-	return foundAccounts;
+	nbrDesComptesTrouvesApresRecheche=insertingIndex;
 
 }
+void addToAccount(int accountId,float amount){
+	comptes[accountId].sold+=amount;
+}
+void afficherMenuAffichage(){
+	printf("\n1>Par Ordre Ascendant");
+	printf("\n2>Par Ordre Descendant");
+	printf("\n3>Par Ordre Ascendant (les comptes bancaires ayant un montant supérieur à un chiffre introduit)");
+	printf("\n4>Par Ordre Descendant (les comptes bancaires ayant un montant supérieur à un chiffre introduit)");
+	printf("\n5>Recherche par CIN");
+	printf("\n6>Retour au menu principale");
+	MENU_AFFICHAGE_DEMANDER_CHOIX:
+	printf("\nVotreChoix: ");
+	lireOption();
 
+	switch(optionChoisis){
+		case 1://Par Ordre Ascendant
+
+		break;
+		case 2://Par Ordre Descendant
+
+		break;
+		case 3://Par Ordre Ascendant (les comptes bancaires ayant un montant supérieur à un chiffre introduit)
+
+		break;
+		case 4://Par Ordre Descendant (les comptes bancaires ayant un montant supérieur à un chiffre introduit
+
+		break;
+		case 5://Recherche par CIN
+
+		break;
+		case 6://Retour au menu principale
+
+		break;
+		default://no valide option is entred
+		goto MENU_AFFICHAGE_DEMANDER_CHOIX;
+	}
+}
+void trierEtDonnerLesComptesParSoldAsc(Compte sortedTable[],int length){
+	 //TODO finish this
+
+}
+void swapComptes(struct Compte *compte1,struct Compte* compte2 ){
+	Compte tempCompte=*compte1;
+	*compte1=*compte2;
+	*compte2=tempCompte;
+}
 void demmanderLechoix(){
 	printf("\nentrez le nombre correspondant a votre choix puis tapez entrer ");
 }
-
 void header(){
 	printf("\n-------------------------Bonjour Dans La Bank Du Maroc------------------------------\n");
 }
